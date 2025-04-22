42b9de48e804e2414aa355ad348df3ae4d095c15
kaio062 <kairobarbosa2007@gmail.com>
2025-04-22 20:00:02 -0300

    salvar barbeiro

--- codigo/funcoes.php
+++ codigo/funcoes.php
@@ -1,10 +1,10 @@
 <?php
 
-function salvarCliente($conexao, $nome, $email, $telefone, $endereco, $data_nascimento, $data_cadastro) {
-    $sql = "INSERT INTO cliente (nome, email, telefone, endereco, data_nascimento, data_cadastro) VALUES (?, ?, ?)";
+function salvar($conexao, $nome, $email, $telefone, $cpf, $data_nascimento, $data_admissao) {
+    $sql = "INSERT INTO barbeiro (nome, email, telefone, $cpf, data_nascimento, data_admissao) VALUES (?, ?, ?)";
     $comando = mysqli_prepare($conexao, $sql);
     
-    mysqli_stmt_bind_param($comando, 'ssssss', $nome, $email, $telefone, $endereco, $data_nascimento, $data_cadastro);
+    mysqli_stmt_bind_param($comando, 'ssssss', $nome, $email, $telefone, $cpf, $data_nascimento, $data_admissao);
     
     $funcionou = mysqli_stmt_execute($comando);
     mysqli_stmt_close($comando);
