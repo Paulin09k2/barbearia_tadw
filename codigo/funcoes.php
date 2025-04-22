162d523a5a11b0dc5d55f5c00f431ac7230d133e
kaio062 <kairobarbosa2007@gmail.com>
2025-04-22 19:52:29 -0300

    mundaça no sss

--- codigo/funcoes.php
+++ codigo/funcoes.php
@@ -4,7 +4,7 @@ function salvarCliente($conexao, $nome, $email, $telefone, $endereco, $data_nasc
     $sql = "INSERT INTO cliente (nome, email, telefone, endereco, data_nascimento, data_cadastro) VALUES (?, ?, ?)";
     $comando = mysqli_prepare($conexao, $sql);
     
-    mysqli_stmt_bind_param($comando, 'sss', $nome, $email, $telefone, $endereco, $data_nascimento, $data_cadastro);
+    mysqli_stmt_bind_param($comando, 'ssssss', $nome, $email, $telefone, $endereco, $data_nascimento, $data_cadastro);
     
     $funcionou = mysqli_stmt_execute($comando);
     mysqli_stmt_close($comando);
