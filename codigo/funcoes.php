756e0bc41c8d5e816a71ae2efcb609d8fc8d3264
kaio062 <kairobarbosa2007@gmail.com>
2025-04-22 20:20:50 -0300

    deletar barbeiro

--- codigo/funcoes.php
+++ codigo/funcoes.php
@@ -15,10 +15,21 @@ function editarbarbeiro($conexao, $nome, $email, $telefone, $cpf, $data_nascimen
     $sql = "UPDATE barbeiro SET nome=?, $email=?, telefone=?, $cpf, data_nascimento=?, data_admissao=? WHERE id_barbeiro=?";
     $comando = mysqli_prepare($conexao, $sql);
     
-    mysqli_stmt_bind_param($comando, 'sssi', $nome, $email, $telefone, $cpf, $data_nascimento, $data_admissao, $id);
+    mysqli_stmt_bind_param($comando, 'ssssss', $nome, $email, $telefone, $cpf, $data_nascimento, $data_admissao, $id);
     $funcionou = mysqli_stmt_execute($comando);
 
     mysqli_stmt_close($comando);
     return $funcionou;    
+}
+function deletarbarbeito($conexao, $id_barbeiro) {
+    $sql = "DELETE FROM barbeiro WHERE id_barbeiro = ?";
+    $comando = mysqli_prepare($conexao, $sql);
+    
+    mysqli_stmt_bind_param($comando, 'i', $id_barbeiro);
+
+    $funcionou = mysqli_stmt_execute($comando);
+    mysqli_stmt_close($comando);
+    
+    return $funcionou;
 }
 ?>
