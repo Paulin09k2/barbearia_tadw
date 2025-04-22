f40bddd5620ce1671359205b4c4b3e7e29b05ba5
kaio062 <kairobarbosa2007@gmail.com>
2025-04-22 20:03:00 -0300

    editar barbeiro

--- codigo/funcoes.php
+++ codigo/funcoes.php
@@ -1,6 +1,6 @@
 <?php
 
-function salvar($conexao, $nome, $email, $telefone, $cpf, $data_nascimento, $data_admissao) {
+function salvarbarbeiro($conexao, $nome, $email, $telefone, $cpf, $data_nascimento, $data_admissao) {
     $sql = "INSERT INTO barbeiro (nome, email, telefone, $cpf, data_nascimento, data_admissao) VALUES (?, ?, ?)";
     $comando = mysqli_prepare($conexao, $sql);
     
@@ -10,5 +10,15 @@ function salvar($conexao, $nome, $email, $telefone, $cpf, $data_nascimento, $dat
     mysqli_stmt_close($comando);
     
     return $funcionou;
+}
+function editarbarbeiro($conexao, $nome, $cpf, $endereco, $id) {
+    $sql = "UPDATE tb_cliente SET nome=?, cpf=?, endereco=? WHERE idcliente=?";
+    $comando = mysqli_prepare($conexao, $sql);
+    
+    mysqli_stmt_bind_param($comando, 'sssi', $nome, $cpf, $endereco, $id);
+    $funcionou = mysqli_stmt_execute($comando);
+
+    mysqli_stmt_close($comando);
+    return $funcionou;    
 }
 ?>
