5daa3c4e22b43d2082349ededd2335ba38e605ae
Paulin09k2 <pauloricardoribeiromaciel@gmail.com>
2025-04-23 07:41:47 -0300

    Update funcoes.php

--- codigo/funcoes.php
+++ codigo/funcoes.php
@@ -66,6 +66,51 @@ function deletaragendamento($conexao, $id_agendamento, $barbeiro_id_barbeiro, $c
 } 
 //----------------------------------------------------------------------------------------------------------------------------------------------------------------------Paulo Ricardo:
 
+function salvarCliente($conexao, $nome, $cpf, $endereco) {
+    $sql = "INSERT INTO tb_cliente (nome, cpf, endereco) VALUES (?, ?, ?)";
+    $comando = mysqli_prepare($conexao, $sql);
+    
+    mysqli_stmt_bind_param($comando, 'sss', $nome, $cpf, $endereco);
+    
+    mysqli_stmt_execute($comando);
+    mysqli_stmt_close($comando);
+    
+}
+
+
+
+function editarCliente($conexao, $nome, $cpf, $endereco, $id) {
+    $sql = "UPDATE tb_cliente SET nome=?, cpf=?, endereco=? WHERE idcliente=?";
+    $comando = mysqli_prepare($conexao, $sql);
+    
+    mysqli_stmt_bind_param($comando, 'sssi', $nome, $cpf, $endereco, $id);
+    $funcionou = mysqli_stmt_execute($comando);
+
+    mysqli_stmt_close($comando);
+    return $funcionou;    
+}
+
+
+
+function deletarCliente($conexao, $idcliente) {
+    $sql = "DELETE FROM tb_cliente WHERE idcliente = ?";
+    $comando = mysqli_prepare($conexao, $sql);
+    
+    mysqli_stmt_bind_param($comando, 'i', $idcliente);
+
+    $funcionou = mysqli_stmt_execute($comando);
+    mysqli_stmt_close($comando);
+    
+    return $funcionou;
+}
+
+
+
+
+
+
+
+
 
 
 ?>
