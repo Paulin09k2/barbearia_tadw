f824d26b038f6677a9ba302051efab83bfc43b9c
kaio062 <kairobarbosa2007@gmail.com>
2025-06-12 14:08:38 -0300

    er

---
+++ codigo/tests/teste_salvarcliente.php
@@ -0,0 +1,11 @@
+<?php
+    require_once "../conexao.php";
+    require_once "../funcoes.php";
+
+    $nome = "Kaio";
+    $cpf = "321.456.235-11";
+    $endereco = "Rua 1";
+
+    salvarCliente($conexao, $nome, $email, $telefone, $endereco, $data_nascimento, $data_cadastro);
+
+?>
