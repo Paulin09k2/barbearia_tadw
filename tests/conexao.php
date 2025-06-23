98dd48582dd70e3774fc57e4d448689ef49b1c67
kaio062 <kairobarbosa2007@gmail.com>
2025-06-23 13:38:56 -0300

    k

--- tests/conexao.php
+++ tests/conexao.php
@@ -6,8 +6,3 @@ $banco = 'barbearia';
 
 $conexao = mysqli_connect($servidor, $usuario, $password, $banco);
 
-if (!$conexao) {
-    die("Erro de conexão: " . mysqli_connect_error());
-} else {
-    echo "Conexão realizada com sucesso!";
-}
