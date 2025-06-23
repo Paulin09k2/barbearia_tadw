596ec4606ece37c10f0264b5d2441f9c73898bba
kaio062 <kairobarbosa2007@gmail.com>
2025-06-23 13:49:22 -0300

    g

--- tests/conexao.php
+++ tests/conexao.php
@@ -1,8 +1,11 @@
 <?php
-$servidor = 'localhost';
-$usuario = 'root';
-$password = '123';
-$banco = 'barbearia';
+// Dados de conexão com o banco de dados
+$host = 'localhost';         // ou o IP do servidor do banco
+$usuario = 'root';           // seu usuário do banco
+$senha = '123';                 // sua senha do banco (vazio por padrão no XAMPP)
+$banco = 'banco';    // substitua pelo nome do seu banco de dados
 
-$conexao = mysqli_connect($servidor, $usuario, $password, $banco);
+// Criação da conexão
+$conexao = new mysqli($host, $usuario, $senha, $banco);
+
 
