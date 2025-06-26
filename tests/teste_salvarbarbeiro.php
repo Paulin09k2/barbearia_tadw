e5cc3318ad0785b696d2c207c5678fe1d593a4e6
kaio062 <kairobarbosa2007@gmail.com>
2025-06-26 09:57:23 -0300

    ajusta os testes de salvarBarbeiro e salvarCliente para corrigir dados e formatação

--- tests/teste_salvarbarbeiro.php
+++ tests/teste_salvarbarbeiro.php
@@ -1,23 +1,21 @@
 <?php
-require_once "conexao.php";      
-require_once "funcoes.php";   
+require_once "conexao.php";
+require_once "funcoes.php";
 
-<<<<<<< HEAD
-$nome = "paulo";
-$email = "gcfdw@gmail.com";
+$nome = "laio";
+$email = "io@gmail.com";
 $telefone = "45545"; 
-$cpf = "32145623511";
+$cpf = "32145623891";
 $data_nascimento = "2000-01-01";
 $data_admissao = "2000-01-02";
-=======
-$nome = "iagoio";
-$email = "kaio@dfkakdail.com";
-$telefone = "87999900000";
-$cpf = "321.456.235-11";
-$data_nascimento = "2000-08-15";
-$data_admissao = "2022-01-10";
->>>>>>> 65d4d3f12220b8438005b5daf57450df89f33ce6
 $senha = "123456";
 
-salvarBarbeiro($conexao, $nome, $email, $telefone, $cpf, $data_nascimento, $data_admissao, $senha);
+// Passe a senha como último parâmetro
+$resultado = salvarBarbeiro($conexao, $nome, $email, $telefone, $cpf, $data_nascimento, $data_admissao, $senha);
+
+if ($resultado) {
+    echo "Barbeiro salvo com sucesso!";
+} else {
+    echo "Erro ao salvar barbeiro.";
+}
 ?>
--- tests/teste_salvarcliente.php
+++ tests/teste_salvarcliente.php
@@ -1,12 +1,11 @@
 <?php
-<<<<<<< HEAD
-    require_once "conexao.php";
-    require_once "funcoes.php";
+require_once "conexao.php";
+require_once "funcoes.php";
 
-    $nome = "paulo";
-    $cpf = "321.456.235-11";
+    $nome = "ulo";
+    $cpf = "321.450.235-11";
     $endereco = "Rua 1";
-    $email = "iyg@gmail.com";
+    $email = "ipo@gmail.com";
     $telefone = "1234567890";
     $data_nascimento = "1990-01-01";
     $data_cadastro = "1999-06-01";
@@ -14,28 +13,5 @@
 
     salvarCliente($conexao, $nome, $email, $telefone, $endereco, $data_nascimento, $data_cadastro, $senha_cliente);
 
-    $resultado = salvarCliente($conexao, $nome, $email, $telefone, $cpf, $data_nascimento, $data_admissao, $senha);
 
-if ($resultado) {
-    echo "Barbeiro salvo com sucesso!";
-} else {
-    echo "Erro ao salvar barbeiro.";
-}
 ?>
-?>
-=======
-require_once "conexao.php";      
-require_once "../funcoes.php";   
-
-
-$nome = "Kaio";
-$email = "kaio@email.com";
-$telefone = "87999998888";
-$endereco = "Rua 1, Bairro Central";
-$data_nascimento = "2000-08-15";
-$data_cadastro = "8900-06-17";
-
-
-salvarCliente($conexao, $nome, $email, $telefone, $endereco, $data_nascimento, $data_cadastro);
-?>
->>>>>>> 65d4d3f12220b8438005b5daf57450df89f33ce6
