65d4d3f12220b8438005b5daf57450df89f33ce6
Paulin09k2 <pauloricardoribeiromaciel@gmail.com>
2025-06-26 09:23:01 -0300

    Update teste_salvarbarbeiro.php

--- tests/teste_salvarbarbeiro.php
+++ tests/teste_salvarbarbeiro.php
@@ -1,21 +1,14 @@
 <?php
-require_once "conexao.php";
-require_once "funcoes.php";
+require_once "conexao.php";      
+require_once "funcoes.php";   
 
-$nome = "Kaio";
-$email = "gcfdw@gmail.com";
-$telefone = "45545"; 
+$nome = "iagoio";
+$email = "kaio@dfkakdail.com";
+$telefone = "87999900000";
 $cpf = "321.456.235-11";
-$data_nascimento = "2000-01-01";
-$data_admissao = "2000-01-02";
+$data_nascimento = "2000-08-15";
+$data_admissao = "2022-01-10";
 $senha = "123456";
 
-
-$resultado = salvarBarbeiro($conexao, $nome, $email, $telefone, $cpf, $data_nascimento, $data_admissao, $senha);
-
-if ($resultado) {
-    echo "Barbeiro salvo com sucesso!";
-} else {
-    echo "Erro ao salvar barbeiro.";
-}
+salvarBarbeiro($conexao, $nome, $email, $telefone, $cpf, $data_nascimento, $data_admissao, $senha);
 ?>
