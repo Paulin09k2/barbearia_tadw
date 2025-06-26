0f7f5daba39db1bf2298368fce1b534a96bb5e87
Paulin09k2 <pauloricardoribeiromaciel@gmail.com>
2025-06-26 08:52:39 -0300

    Update teste_salvarcliente.php

--- tests/teste_salvarcliente.php
+++ tests/teste_salvarcliente.php
@@ -1,11 +1,15 @@
 <?php
-    require_once "conexao.php";
-    require_once "../funcoes.php";
+require_once "conexao.php";      
+require_once "../funcoes.php";   
 
-    $nome = "Kaio";
-    $cpf = "321.456.235-11";
-    $endereco = "Rua 1";
 
-    salvarCliente($conexao, $nome, $email, $telefone, $endereco, $data_nascimento, $data_cadastro);
+$nome = "Kaio";
+$email = "kaio@email.com";
+$telefone = "87999998888";
+$endereco = "Rua 1, Bairro Central";
+$data_nascimento = "2000-08-15";
+$data_cadastro = "8900-06-17";
 
+
+salvarCliente($conexao, $nome, $email, $telefone, $endereco, $data_nascimento, $data_cadastro);
 ?>
