f059f3b11d7ed1da3c7fed9088cdcfac7feeae2a
kaio062 <kairobarbosa2007@gmail.com>
2025-08-07 16:14:32 -0300

    few

---
+++ tests/teste_editarAgendamento.php
@@ -0,0 +1,14 @@
+<?php
+require_once "../conexao.php";
+require_once "../funcoes.php";
+
+$data_agendamento = "2025-09-02 10:00:00";
+$status = "confirmado";
+$barbeiro_id = 1;
+$cliente_id = 1;
+$id_agendamento = 1;
+
+$resultado = editarAgendamento($conexao, $data_agendamento, $status, $barbeiro_id, $cliente_id, $id_agendamento);
+
+echo $resultado;
+?>
