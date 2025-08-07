9b43441e315f9ec8d83b1930482e87448cddd4e6
Paulin09k2 <pauloricardoribeiromaciel@gmail.com>
2025-08-07 16:21:18 -0300

    Create teste_editarAvaliacao.php

---
+++ tests/teste_editarAvaliacao.php
@@ -0,0 +1,16 @@
+<?php
+require_once "../conexao.php";
+require_once "../funcoes.php";
+
+$id = 1; // ID real da avaliação existente
+$estrela = 4;
+$comentario = "Bom, mas pode melhorar.";
+$barbeiro_id_barbeiro = 1;
+$servico_id_servico = 1;
+
+if (editarAvaliacao($conexao, $estrela, $comentario, $barbeiro_id_barbeiro, $servico_id_servico, $id)) {
+    echo "Avaliação editada com sucesso!";
+} else {
+    echo "Erro ao editar avaliação.";
+}
+?>
