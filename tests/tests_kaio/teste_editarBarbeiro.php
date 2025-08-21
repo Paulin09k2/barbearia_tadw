d3714e125a076ea9e27c6f14a6ce76130237c6d9
Paulin09k2 <pauloricardoribeiromaciel@gmail.com>
2025-08-21 16:12:22 -0300

    d

--- tests/tests_kaio/teste_editarBarbeiro.php
+++ tests/tests_kaio/teste_editarBarbeiro.php
@@ -10,7 +10,7 @@ $data_nascimento = "2000-01-01";
 $data_admissao = "2000-01-02";
 $id_barbeiro = 6;
 
-$resultado = editarBarbeiro($conexao, $nome, $email, $telefone, $cpf, $data_nascimento, $data_admissao, $id_barbeiro);
+$resultado = editarBarbeiro($conexao, $nome, $email, $telefone, $cpf, $data_nascimento, $id_barbeiro);
 
 echo $resultado ? "Barbeiro editado com sucesso!" : "Erro ao editar barbeiro.";
 ?>
