482088502678d101ae870a7e6e0c25727af45d03
Paulin09k2 <pauloricardoribeiromaciel@gmail.com>
2025-08-28 16:45:51 -0300

    Update teste_salvaCliente.php

--- tests/teste_salvaCliente.php
+++ tests/teste_salvaCliente.php
@@ -5,11 +5,11 @@
     $nome = "Fulano";
     $cpf = "321.456.234-11";
     $telefone = "(11) 91234-5678";
-    $email = ""
+    $email = "fulano@example.com";
     $data_nascimento = "1990-01-01";
     $data_cadastro = "2023-10-01";
     $senha_cliente = "senha123";
     $endereco = "Rua 1";
 
-    salvarCliente($conexao, $nome, $cpf, $endereco);
+    salvarCliente($conexao,$nome, $email, $telefone, $endereco, $data_nascimento, $data_cadastro, $senha_cliente);
 ?>
