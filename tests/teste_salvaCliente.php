5bcc2bbe8e3aceac1a86f9caa32b76d427af0439
kaio062 <kairobarbosa2007@gmail.com>
2025-08-28 16:34:32 -0300

    Atualiza caminhos de inclusão em formGeral.php e adiciona teste para salvar cliente

---
+++ tests/teste_salvaCliente.php
@@ -0,0 +1,15 @@
+<?php
+    require_once "../conexao.php";
+    require_once "../funcoes.php";
+
+    $nome = "Fulano";
+    $cpf = "321.456.234-11";
+    $telefone = "(11) 91234-5678";
+    $email = ""
+    $data_nascimento = "1990-01-01";
+    $data_cadastro = "2023-10-01";
+    $senha_cliente = "senha123";
+    $endereco = "Rua 1";
+
+    salvarCliente($conexao, $nome, $cpf, $endereco);
+?>
