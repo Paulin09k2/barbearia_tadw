4d2d9b4c2945bab3e0e14169ac7543a67dc0bb90
kaio062 <kairobarbosa2007@gmail.com>
2025-09-11 08:05:07 -0300

    gt

---
+++ ideias/formCliente.php
@@ -0,0 +1,39 @@
+<?php
+require_once "../tests/conexao.php";
+require_once "../tests/funcoes.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+      <h2>Cadastrar Cliente</h2>
+    <form method="post" action="processa.php">
+        <input type="hidden" name="acao" value="salvarCliente">
+        Nome: <input type="text" name="nome" required><br>
+        Email: <input type="email" name="email" required><br>
+        Telefone: <input type="text" name="telefone" required><br>
+        Endereço: <input type="text" name="endereco" required><br>
+        Data de Nascimento: <input type="date" name="data_nascimento" required><br>
+        Data de Cadastro: <input type="date" name="data_cadastro" required><br>
+        Senha: <input type="password" name="senha_cliente" required><br>
+        <button type="submit">Salvar Cliente</button>
+    </form>
+
+    <h3>Clientes cadastrados</h3>
+    <ul>
+        <?php
+        $clientes = listarCliente($conexao);
+        foreach($clientes as $c){
+            echo "<li>ID: {$c['id_cliente']} | Nome: {$c['nome']} | Email: {$c['email']}</li>";
+        }
+        ?>
+    </ul>
+
+    <hr>
+</body>
+</html>
--- tests/teste_salvarBarbeiro.php
+++ tests/teste_salvarBarbeiro.php
@@ -2,13 +2,13 @@
 require_once "conexao.php";
 require_once "funcoes.php";
 
-$nome = "Kaio";
-$email = "kaio@gmail.com";
-$telefone = "645165651"; 
-$cpf = "11122233344";
+$nome = "Kaio3";
+$email = "kaio2@gmail.com";
+$telefone = "645168651"; 
+$cpf = "11122293344";
 $data_nascimento = "2000-01-01";
 $data_admissao = "2000-01-02";
-$senha = "123456";
+$senha = "123459";
 
 // Passe a senha como último parâmetro
 $resultado = salvarBarbeiro($conexao, $nome, $email, $telefone, $cpf, $data_nascimento, $data_admissao, $senha);
