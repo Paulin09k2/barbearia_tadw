b77708d05ee567f6830a2085610034fc8cdb298f
kaio062 <kairobarbosa2007@gmail.com>
2025-09-15 14:42:14 -0300

    adiciona arquivo de formulário de login (login.php)

---
+++ ideias/login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <title>Login</title>
+</head>
+<body>
+  <form action="" method="POST">
+    <h2>Login</h2>
+    <label for="usuario">Usuário</label>
+    <input type="text" id="usuario" name="usuario" required /><br>
+    <br>
+    <label for="senha">Senha</label>
+    <input type="password" id="senha" name="senha" required /><br><br>
+    
+    <button type="submit">Entrar</button><br>
+  </form>
+</body>
+</html>
