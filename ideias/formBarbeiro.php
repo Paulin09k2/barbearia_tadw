a81a389cc59bc643efc44881db781e3d8f4b2eba
kaio062 <kairobarbosa2007@gmail.com>
2025-09-29 13:21:59 -0300

    f

--- ideias/formBarbeiro.php
+++ ideias/formBarbeiro.php
@@ -8,6 +8,7 @@ require_once "../tests/funcoes.php";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="login" href="logo3.png">
     <title>Document</title>
 </head>
 <body>
