d3a4827f21963b4c1927e7c6ab76dc79faf1ea54
kaio062 <kairobarbosa2007@gmail.com>
2025-10-09 09:36:45 -0300

    Add function to search for client by ID and create activity log for project tasks

--- tests/funcoes.php
+++ tests/funcoes.php
@@ -215,4 +215,17 @@ function salvarAvaliacao($conexao, $estrela, $comentario, $barbeiro_id_barbeiro,
     mysqli_stmt_close($stmt);
     return $ok;
 }
+
+function pesquisarClienteId($conexao, $id_cliente) {
+    $sql = "SELECT * FROM cliente WHERE id_cliente = ?";
+    $comando = mysqli_prepare($conexao, $sql);
+    mysqli_stmt_bind_param($comando, 'i', $id_cliente);
+    mysqli_stmt_execute($comando);
+    $resultado = mysqli_stmt_get_result($comando);
+    $cliente = mysqli_fetch_assoc($resultado);
+    mysqli_stmt_close($comando);
+    return $cliente;
+}
 ?>
+
+
