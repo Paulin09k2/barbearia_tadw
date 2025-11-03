27f020a37805a81803c425eaf13bc32a297523aa
kaio062 <kairobarbosa2007@gmail.com>
2025-11-03 15:05:54 -0300

    mk

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Barbearia Elite</title>
-  <link rel="icon" type="login" href="logo.png">
+  <link rel="shortcut icon" href="logo.png" type="image/png">
   <script src="https://cdn.tailwindcss.com"></script>
   <script>
     tailwind.config = {
